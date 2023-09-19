a7f16fde32dc000a6384d8a61a07a03a02c7b597
MiraeKim <hufs202000512@gmail.com>
2023-09-19 22:59:49 +0900

    B5_4101

--- Bronze/Bronze5/B5_4101.java
+++ Bronze/Bronze5/B5_4101.java
@@ -1,9 +1,6 @@
 package Bronze.Bronze5;
 import java.util.Scanner;
 
-// 첫 번째 수가 두번째 수보다 큰지 구하는 프로그램
-// 0 0이 들어오면 종료
-
 public class B5_4101 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
