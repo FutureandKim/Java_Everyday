6ec8871609977989394a7f50304343f57a9323e8
MiraeKim <hufs202000512@gmail.com>
2023-09-20 20:27:37 +0900

    9.20_success_3

---
+++ Bronze/Bronze5/B5_14645.java
@@ -0,0 +1,18 @@
+package Bronze.Bronze5;
+import java.util.Scanner;
+
+public class B5_14645 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i=0;i<N;i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+        }
+        System.out.println("비와이");
+        sc.close();
+    }
+    
+}
---
+++ Bronze/Bronze5/B5_30030.java
@@ -0,0 +1,11 @@
+package Bronze.Bronze5;
+import java.util.Scanner;
+
+public class B5_30030 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int B = sc.nextInt();
+        System.out.println(10*B/11);
+        sc.close();
+    }
+}
